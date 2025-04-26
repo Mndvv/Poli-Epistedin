4afb096f5988f6285cf4c40b6993ddbff989990b
Mndvv <juansyahakbar25@gmail.com>
2025-04-27 00:55:40 +0700

    Add smaller font options for more responsiveness

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className="flex flex-col items-center justify-center w-full p-4 mt-8 bg-transparent max-w-3xl mb-5 md:mb-0 mx-auto gap-8">
             <div>
-            <p className="text-sm md:text-base text-center text-gray-500">Juansyah Akbar © 2025 Licensed in <Link href="https://github.com/Mndvv/CoSenseOP-License-V1.0" className="border-b border-gray-500">CoSenseOP-License-V1.0.</Link></p>
+            <p className="text-sm sm:text-xs md:text-base text-center text-gray-500">Juansyah Akbar © 2025 Licensed in <Link href="https://github.com/Mndvv/CoSenseOP-License-V1.0" className="border-b border-gray-500">CoSenseOP-License-V1.0.</Link></p>
             </div>
         </footer>
     );
