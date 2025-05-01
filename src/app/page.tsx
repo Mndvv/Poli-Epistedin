6d73c92f3d08ec4cb9598f27d115ce42921f740a
Mndvv <juansyahakbar25@gmail.com>
2025-05-01 23:55:11 +0700

    Wrapping navbar in Suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import VerticalNavbar from '@/components/navbar';
 import banner from '@assets/img/banner-2.jpg';
 
 import { Metadata } from 'next';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
   title: 'Landing - Poli-Epistedin',
@@ -19,7 +20,9 @@ export default function Home() {
   return (
     <div className='p-6'>
       <ProfileCard />
-      <VerticalNavbar />
+      <Suspense fallback={<div>Loading...</div>}>
+        <VerticalNavbar />
+      </Suspense>
     </div>
   );
 }
