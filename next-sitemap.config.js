b545b75f7c7875c6b1c170b4658cbd49a69f8e20
Mndvv <juansyahakbar25@gmail.com>
2025-05-07 10:36:09 +0700

    Improve Site For Google Search Indexing

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://saintark.my.id', // ← your site URL (important)
+  generateRobotsTxt: true, // ← generates robots.txt too
+  sitemapSize: 5000,
+};
