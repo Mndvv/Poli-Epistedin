8d4b550b17312b63b730aa4ecea90df791db6a7a
Stark <95195608+Mndvv@users.noreply.github.com>
2025-05-07 14:43:41 +0700

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://saintark.my.id', // ← your site URL (important)
+  siteUrl: 'https://www.saintark.my.id', // ← your site URL (important)
   generateRobotsTxt: true, // ← generates robots.txt too
   sitemapSize: 5000,
 };
